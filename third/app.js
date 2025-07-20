9730847e52329b6d18693d4801eceb4e7788b930
jayad <jayadpathandev@gmail.com>
2025-07-20 18:59:16 +0530

    Refactor product management in AngularJS app: implement add and delete product functionalities in app.js; update index.html for improved structure and user interaction.

--- third/app.js
+++ third/app.js
@@ -3,4 +3,21 @@ const app = angular.module("myApp",[]);
 app.controller("myController", function($scope){
     // Proudct list
     $scope.products = ["Laptop", "Mobile", "Tablet"];
+
+    // Add new product
+    $scope.addProduct = function(){
+        if($scope.newProduct && !$scope.products.includes($scope.newProduct)){
+            $scope.products.push($scope.newProduct);
+            $scope.newProduct = "";
+        }
+    }
+
+    // Remove product
+    $scope.deleteProduct = function(product){
+        for(var i=0; i<$scope.products.length; i++){
+            if($scope.products[i] == product){
+                $scope.products.splice(i, 1);
+            }
+        }
+    }
 })
